8d1ca5724d1261aac6de22154bf6908e2a46d3b4
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2024-01-10 08:22:19 -0700

    Agregar archivos de proyecto.

---
+++ Practica1_ASA/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practica1_ASA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Refri.IsRefreshing = true;
+            Thread.Sleep(3000);
+            Refri.IsRefreshing = false;
+
+        }
+    }
+}
